4d92a43f9fea6bce637635dd1b68fc5fe512ef89
tdanh2k <tdanh2k@gmail.com>
2020-11-24 16:54:40 +0700

    Revert "Initial commit"
    This reverts commit 7e4a49c49371e4e0113301b7f347433a0c149007.

--- PRO2112_Final/src/main/java/com/example/repository/dataTablesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
-
-import com.example.entity.phpTest;
-
-public interface dataTablesRepository extends DataTablesRepository<phpTest, Integer>{
-
-}
